1c47b421c561bb64794f97a382267fc362e3e11b
Duxql <telesp319@gmail.com>
2024-03-05 20:09:00 +0000

    Update Program.cs

--- CMP1903_A1_2324/Program.cs
+++ CMP1903_A1_2324/Program.cs
@@ -15,8 +15,13 @@ namespace CMP1903_A1_2324
              * Create a Testing object to verify the output and operation of the other classes.
              */
 			 
+			 //Roll the dices and sum them up
 			 Game.DiceTotal();
+			 
+			 //Test that it's correctly rolling dices
 			 Testing.testDie();
+			 
+			 //Test that it's currently summing up the dices
 			 Testing.testSum();
         }
     }
