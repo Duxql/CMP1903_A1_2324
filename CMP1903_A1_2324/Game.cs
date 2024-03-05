2015a32a31ccf9cf6d5e4d9bb95769e3c3c39625
Duxql <telesp319@gmail.com>
2024-03-05 18:57:48 +0000

    Update Game.cs

--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -14,8 +14,22 @@ namespace CMP1903_A1_2324
          * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
          * rolls could be continous, and the totals and other statistics could be summarised for example.
          */
-
+		
         //Methods
+		public static int DiceTotal() 
+        {
+            Console.WriteLine("I will attempt to make a dice and roll it");
+            Die myDieOne = new Die(0);
+			myDieOne.Value = Die.Roll();
+			Die myDieTwo = new Die(0);
+			myDieTwo.Value = Die.Roll();
+			Die myDieThree = new Die(0);
+			myDieThree.Value = Die.Roll();
+          	int sum = 0;
+            sum = myDieOne.Value + myDieTwo.Value + myDieThree.Value;
+    
+            return sum;
+        }
 
     }
 }
