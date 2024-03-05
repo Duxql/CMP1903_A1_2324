93402f465b3d85b740c6b3fa63eed60efb402eac
Duxql <telesp319@gmail.com>
2024-03-05 19:40:40 +0000

    Update Die.cs

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -14,6 +14,7 @@ namespace CMP1903_A1_2324
          */
 
         //Property
+		//Create the die object property with a value so it can be assigned a value
 		public int Value{get; set;}
         public Die(int value)
         {
@@ -21,11 +22,14 @@ namespace CMP1903_A1_2324
         }
 		
 		//method
+		//create a method that generates a random number 1-6 and it returns that value to anything that calls this method
 		public static int Roll()
         {
-			//Console.WriteLine("The code is still working.");
+			//Console.WriteLine("The code is still working.")
 			Random rnd = new Random();
+			//randomise the number
 			int dieNumber = rnd.Next(1, 7);
+			//return the number
 			return dieNumber;
         }
     }
